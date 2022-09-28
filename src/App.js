e3d87dc8c897896430861fe603fc555fa1b5e2d5
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-28 16:14:18 -0300

    feat: create router structure in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
+import Register from "./pages/auth/Register";
 
 function App() {
-  return;
-  <>
-    <GlobalStyle />
-  </>;
+  return (
+    <>
+      <GlobalStyle />
+
+      <UserContext.Provider value={""}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={""}></Route>
+            <Route path="/signup" element={<Register />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </>
+  );
 }
 
 export default App;
