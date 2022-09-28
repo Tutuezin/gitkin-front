091ab0bad821eda00ea2adfcd67cc694cc90b8d6
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-28 16:06:35 -0300

    feat: create register component

---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>.</div>
+      <div>.</div>
+    </>
+  );
+}
