23418d89dbeb761ebeb5eda82f71dd3d41b2aa09
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-28 17:53:13 -0300

    feat: register layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  font-family: "Poppins", sans-serif;
+
+  display: flex;
+  align-items: center;
+
+  height: 8.5rem;
+  border-bottom: 0.1rem solid #837e9f;
+  padding-left: 5rem;
+
+  color: #837e9f;
+  font-size: 35px;
+  font-weight: 700;
+`;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -6,20 +6,106 @@ export default function Register() {
   const navigate = useNavigate();
   return (
     <>
-      <Header>GitKin</Header>
+      <Container>
+        <AuthInputs>
+          <h2>Crie sua conta</h2>
+          <Form>
+            <Input type="email" placeholder="Seu e-mail"></Input>
+            <Input type="text" placeholder="Seu nome"></Input>
+            <Input type="password" placeholder="Sua senha"></Input>
+            <Input type="password" placeholder="Confirme sua senha"></Input>
+          </Form>
+          <p>
+            Ao se registrar, você aceita nossos <span>termos de uso</span> e a
+            nossa <span>política de privacidade</span>.
+          </p>
+
+          <Button>Cadastrar</Button>
+        </AuthInputs>
+      </Container>
     </>
   );
 }
 
-export const Header = styled.header`
+export const Container = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
 
-  height: 8.5rem;
-  border-bottom: 0.1rem solid #837e9f;
-  padding-left: 5rem;
+export const AuthInputs = styled.div`
+  min-height: 66rem;
+  min-width: 45.6rem;
+  margin-top: 24.6rem;
 
-  color: #837e9f;
-  font-size: 35px;
-  font-weight: 700;
+  background-color: #302f3d;
+  border-radius: 2rem;
+  padding: 6.4rem;
+
+  h2 {
+    font-family: "Merriweather Sans", sans-serif;
+    font-size: 2.5rem;
+    font-weight: 400;
+    color: #fff;
+    margin-bottom: 7.1rem;
+  }
+
+  p {
+    max-width: 32.7rem;
+    text-align: center;
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    color: #fff;
+  }
+  span {
+    color: #837e9f;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  display: flex;
+
+  min-height: 5rem;
+  min-width: 32.7rem;
+  padding-left: 15px;
+  margin-bottom: 2rem;
+
+  border-radius: 0.5rem;
+  border: none;
+  background-color: #22212c;
+
+  font-family: "Poppins", sans-serif;
+  font-size: 1.6rem;
+  color: #fff;
+
+  &::placeholder {
+    font-family: "Poppins", sans-serif;
+    opacity: 1;
+    color: #fff;
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+
+  outline: none; //TODO fazer uma transicao ao clicar no input
+`;
+
+export const Button = styled.button`
+  font-family: "Merriweather Sans", sans-serif;
+  font-size: 2.3rem;
+  color: #fff;
+
+  border: none;
+  border-radius: 0.5rem;
+
+  margin-top: 3.5rem;
+  width: 32.7rem;
+  height: 5rem;
+
+  background-color: #837e9f;
 `;
