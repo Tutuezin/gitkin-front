0046e4396876323bb2ce9ef118f5442829726aae
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-04 20:10:40 -0300

    chore: install react toastify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
