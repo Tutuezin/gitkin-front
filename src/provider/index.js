772216202018c070b108b619b6d37c1d9356d6d4
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-09 01:03:37 -0300

    refactor: create auth provider

---
+++ src/provider/index.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useReducer, useRef } from "react";
+import { buildActions } from "./build-actions";
+import { reducer } from "./reducer";
+
+const initialState = {
+  name: "",
+  picture: "",
+  occupation: "",
+  memberSince: "",
+  aboutMe: "",
+  location: "",
+  work: "",
+  github: "",
+  linkedin: "",
+  twitter: "",
+  website: "",
+  email: "",
+  stringAvatar: "",
+};
+
+const Context = createContext(null); //TODO Pesquisar como tipar este createContext
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const actions = useRef(buildActions(dispatch));
+
+  return (
+    <Context.Provider value={[state, actions.current]}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(Context);
+
+  return context;
+};
