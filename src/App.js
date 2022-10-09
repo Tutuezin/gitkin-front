9da75d29d68e8e0f80e506c40723dbb87b68332e
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-09 01:04:11 -0300

    refactor: set auth provider on app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import Portfolio from "./pages/home/Portfolio";
 import { useState } from "react";
+import { AuthProvider } from "./provider";
 
 function App() {
   const [token, setToken] = useState("");
@@ -16,14 +17,16 @@ function App() {
       <GlobalStyle />
 
       <UserContext.Provider value={contextValue}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={""}></Route>
-            <Route path="/signin" element={<Login />}></Route>
-            <Route path="/signup" element={<Register />}></Route>
-            <Route path="/:username" element={<Portfolio />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={""}></Route>
+              <Route path="/signin" element={<Login />}></Route>
+              <Route path="/signup" element={<Register />}></Route>
+              <Route path="/:username" element={<Portfolio />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
       </UserContext.Provider>
     </>
   );
