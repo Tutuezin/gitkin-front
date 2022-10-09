6a0b75450e8712e7687b8cf29d2dddb576b78c18
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-09 00:52:23 -0300

    refactor: leaves the splitString function in a different file

--- src/utils/homeUtils.js
+++ src/utils/homeUtils.js
@@ -1,10 +1,11 @@
-export const splitString = (name, setStringAvatar) => {
-  let strArr = name;
-  strArr = strArr.split(" ");
-
-  if (strArr.length === 2 && strArr[1].length > 0) {
-    setStringAvatar(`${name.split(" ")[0][0]} ${name.split(" ")[1][0]}`);
-  } else {
-    setStringAvatar(`${name.split(" ")[0][0]}`);
+export const splitString = (name, setName) => {
+  if (name) {
+    let strArr = name;
+    strArr = strArr.split(" ");
+    if (strArr.length === 2 && strArr[1].length > 0) {
+      return setName(`${name.split(" ")[0][0]} ${name.split(" ")[1][0]}`);
+    } else {
+      return setName(`${name.split(" ")[0][0]}`);
+    }
   }
 };
