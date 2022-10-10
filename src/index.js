af814c43904e22451320472a891ab772cf88375f
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-10-10 20:30:02 -0300

    chore: add favicon

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.min.css";
 import "react-toastify/dist/ReactToastify.css";
+import Favicon from "react-favicon";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Favicon url="https://cdn.discordapp.com/attachments/478000281355943936/1029173825583202334/GitKin-1-removebg-preview2.png" />
     <App />
   </React.StrictMode>
 );
